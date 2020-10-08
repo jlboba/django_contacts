f236371e8337376a910dd13ffbd96ac503d831e4
Jerrica Bobadilla <jerrica@Jerricas-iMac-5.home>
2020-10-08 11:33:18 -0700

    configure for heroku

--- django_contacts/settings.py
+++ django_contacts/settings.py
@@ -56,7 +56,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'django_contacts.urls'
 
