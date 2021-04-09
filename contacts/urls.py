776cbe384461ef41547e2e4ba68c1f7b8a7ca788
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-04-09 11:43:30 -0600

    added about and profile views

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact_list, name='contact_list'),
+    path('contact_list', views.contact_list, name='contact_list'),
     path('contacts/<int:pk>', views.contact_detail, name='contact_detail'),
     path('contacts/new', views.contact_create, name='contact_create'),
     path('contacts/<int:pk>/edit', views.contact_edit, name='contact_edit'),
-    path('contacts/<int:pk>/delete', views.contact_delete, name='contact_delete')
+    path('contacts/<int:pk>/delete', views.contact_delete, name='contact_delete'),
+    path('', views.profile, name='profile'),
+    path('about', views.about, name='about'),
 ]
